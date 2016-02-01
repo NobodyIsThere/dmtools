63ed31cb02632bd30e9cd7494a1e2db39c0b890b
gumptiousCreator <hookaduckling@hotmail.com>
2016-02-01 23:47:49 +0000

    Perlin noise for continents and coastlines.
    Need to tweak the parameters to get scales right.

--- worldgen.py
+++ worldgen.py
@@ -1,8 +1,15 @@
 from collections import deque
 import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
+from noise import snoise2
 import numpy as np
 from scipy.spatial import Voronoi, voronoi_plot_2d
+
+MAP_WIDTH = 2.
+MAP_HEIGHT = 1.
+
+CONTINENT_SCALE = 2
+DETAIL = 0.5
 
 OCEAN = 0
 BARE = 1
@@ -18,20 +25,21 @@ class Region(object):
         self.coords = coords
         self.vertices = vertices
         self.elevation = 0
-        self.vertex_elevations = []
+        self.vertex_elevations = [0]*len(vertices)
         self.water = False
         self.biome = BARE
         self.neighbours = []
         self.visited = False
 
 def generate(num_points,
-             num_mountains,
-             mountain_determination=0.8,
-             mountain_dropoff=0.9,
-             elevation_noise=0.1,
-             landgen_iterations=10,
-             water_level=0):
-    points = np.random.uniform(size=(num_points,2))
+             perlin_octaves=10,
+             perlin_persistence=0.8,
+             perlin_lacunarity=2.0,
+             water_level=0,
+             seed=0):
+    xpoints = np.random.uniform(high=MAP_WIDTH, size=num_points)
+    ypoints = np.random.uniform(high=MAP_HEIGHT, size=num_points)
+    points = zip(xpoints, ypoints)
     vor = Voronoi(points)
     
     region_dict = {}
@@ -55,26 +63,25 @@ def generate(num_points,
         regions.append(region)
     
     # Now do elevation
-    for i in range(num_mountains):
-        start_index = np.random.randint(len(regions))
-        direction = np.random.uniform(low=-1., high=1., size=2)
-        regions[start_index].elevation = 1
-        for r in regions[start_index].neighbours:
-            r.elevation = 1
-        queue = deque([regions[start_index]])
-        while len(queue) > 0:
-            region = queue.pop()
-            generate_mountains(region, direction, mountain_determination,
-                mountain_dropoff, elevation_noise)
-            queue.extend([n for n in region.neighbours if not n.visited])
-            region.visited = True
-        for region in regions:
-            region.visited = False
-            if region.elevation > 1:
-                region.elevation = 1
-            region.elevation += water_level
-            if region.elevation < 0:
-                region.biome = OCEAN
+    for region in regions:
+        for i, vertex in enumerate(region.vertices):
+            # Large-scale features
+            region.vertex_elevations[i] = \
+                snoise2(CONTINENT_SCALE*vertex[0],
+                    CONTINENT_SCALE*vertex[1],
+                    octaves=1, base=2*seed, repeatx=MAP_WIDTH*CONTINENT_SCALE)
+            region.vertex_elevations[i] += \
+                DETAIL*snoise2(vertex[0], vertex[1],
+                    octaves=perlin_octaves, persistence=perlin_persistence,
+                    lacunarity=perlin_lacunarity,
+                    base=seed, repeatx=MAP_WIDTH)
+        region.elevation = np.mean(region.vertex_elevations)
+    
+    for region in regions:
+        if region.elevation > 1:
+            region.elevation = 1
+        if region.elevation < water_level:
+            region.biome = OCEAN
     
     draw(regions)
 
@@ -100,4 +107,6 @@ def draw(regions):
                 else colors[region.biome],
             edgecolor='none')
         plt.gca().add_patch(p)
+    plt.xlim([0, MAP_WIDTH])
+    plt.ylim([0, MAP_HEIGHT])
     plt.show()
