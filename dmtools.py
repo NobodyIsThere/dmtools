0d692d3bed1fe08be8c3e324b8d981596f04b977
gumptiousCreator <hookaduckling@hotmail.com>
2016-02-28 08:31:30 +0000

    Random NPC race and alignment.

--- dmtools.py
+++ dmtools.py
@@ -89,6 +89,43 @@ class DMTools(cmd.Cmd):
     def do_n(self, string):
         self.do_next(string)
         
+    def do_npc(self, string):
+        # Race and sex of random NPC in a human-dominated area
+        law = roll.parse("d3")
+        if law == 1:
+            law = "L"
+        else:
+            law = "C" if law == 2 else "N"
+        moral = roll.parse("d3")
+        if moral == 1:
+            moral = "G"
+        else:
+            moral = "E" if moral == 2 else "N"
+        sex = roll.parse("d2")
+        sex = "Male" if sex == 1 else "Female"
+        race = roll.parse("d20")
+        if race < 15:
+            race = "Human"
+        elif race < 17:
+            race = "Dwarf"
+        elif race == 17:
+            race = "Halfling"
+        elif race == 18:
+            race = "Gnome"
+        elif race == 19:
+            race = "Elf"
+        else:
+            race = roll.parse("d10")
+            if race < 7:
+                race = "Half-elf"
+            elif race < 9:
+                race = "Half-orc"
+            elif race == 9:
+                race = "Dragonborn"
+            else:
+                race = "Tiefling"
+        print "%s %s (%s%s)"%(sex, race, law, moral)
+            
     def do_previous(self, string):
         if self.previous:
             print self.previous["description"]
